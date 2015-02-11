b1d7e84a9287c8c72a8f74ac558d600a69851109
aaltzi <aaltzibar007@gmail.com>
2015-02-11 16:20:25 +0100

    rediseño

--- public/js/usuario.js
+++ public/js/usuario.js
@@ -1,25 +1,16 @@
 $(document).ready(function(){
-
     $( "#formname" ).submit(function() {
-
-        console.log("submit");
-
-
         var formData = $(this).serializeArray();
-
         $.ajax({
-           // type: "GET",
            type: "POST",
            url: "/user",
            //dataType: "json",
            dataType: "html",
            data: formData,
-
            success: function(data){
               var msg="Hola, bienvenido ";
               var nombre=document.getElementById('nombre').value;
               $( "#contentDiv" ).html(msg+nombre);
-              
            }
         });
         return false;
